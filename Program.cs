2ae178c73a137e009053ae6e182f620cdd7b0ccf
Yaroslav <yarikgnatuk78@gmail.com>
2019-09-14 16:02:58 +0300

    add Hello World

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace matrix_vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        for(int i = 0 ; i<10; i++)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
